3605c81f314fb402dc5d072e169a90806fe418f1
ak-nkmr <centervil.en@gmail.com>
2020-03-22 23:47:54 +0900

    プロジェクト ファイルを追加します。

---
+++ NavigationServiceTest/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace NavigationServiceTest
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        [System.STAThreadAttribute()]
+        static public void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Startup += App_Startup;
+            app.Run();
+        }
+
+        private static void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            var navigationServiceEx = new NavigationServiceEx(mainWindow);
+            var viewModelFactory = new ViewModelFactory(navigationServiceEx);
+            mainWindow.Show();
+        }
+    }
+}
